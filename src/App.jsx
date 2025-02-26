b86eb3cfeeba7bf57d185bd2eee064eba4b02fd7
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-26 18:14:05 +0100

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
+import Films from './Data/Films'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+const [filterFilm,setFilterFilm]=useState(Films)
+const [genereFilm,setGenereFilm]=useState("")
+useEffect(()=>{
+ if (genereFilm!=="") {
+  const FilmFiltrati=Films.filter((film)=> film.genre===genereFilm)
+  setFilterFilm(FilmFiltrati)
+ }else{
+  setFilterFilm(Films)
+ }
+},[genereFilm])
   return (
     <>
-  
+   <select name="" id="" onChange={(e)=>{setGenereFilm(e.target.value)}}>
+    <option value="">Tutti i film</option>
+    <option value="Fantascienza">Fantascienza</option>
+    <option value="Thriller">Thriller</option>
+    <option value="Romantico">Romantico</option>
+   </select>
+    <ul>
+      {
+       filterFilm.map((film,i)=>{ 
+        return(<li key={i}>{film.title} - {film.genre}</li>) 
+        })
+      }
+      </ul>
     </>
   )
 }
