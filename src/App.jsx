70453dc5d6da8b32dcdfa6ee282edfd56d7f3eda
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-26 18:48:33 +0100

    add bonus

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,35 @@ function App() {
 
 const [filterFilm,setFilterFilm]=useState(Films)
 const [genereFilm,setGenereFilm]=useState("")
+const [title,setTitle]=useState("")
+
 useEffect(()=>{
  if (genereFilm!=="") {
-  const FilmFiltrati=Films.filter((film)=> film.genre===genereFilm)
-  setFilterFilm(FilmFiltrati)
+  const ricercaGenere=Films.filter((film)=> film.genre===genereFilm)
+  setFilterFilm(ricercaGenere)
  }else{
   setFilterFilm(Films)
  }
 },[genereFilm])
+useEffect(()=>{
+  if(title!==""){
+    const ricercaTitolo = Films.filter((film)=>film.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()))  
+    console.log(title)
+    setFilterFilm(ricercaTitolo)
+  }else{
+    setFilterFilm(Films)
+  }
+},[title])
+
   return (
     <>
+    <input type="text" onChange={(e)=>{setTitle(e.target.value)}} name="" id="" />
    <select name="" id="" onChange={(e)=>{setGenereFilm(e.target.value)}}>
     <option value="">Tutti i film</option>
     <option value="Fantascienza">Fantascienza</option>
     <option value="Thriller">Thriller</option>
     <option value="Romantico">Romantico</option>
+    <option value="Azione">Azione</option>
    </select>
     <ul>
       {
